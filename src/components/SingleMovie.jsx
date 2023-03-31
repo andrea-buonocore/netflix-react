30ef08d813e008a625a4f6788c1ea1c4867a959b
andrea-buonocore <123959335+andrea-buonocore@users.noreply.github.com>
2023-03-31 14:56:04 +0200

    due cagate

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,7 +1,8 @@
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import { Component } from "react";
-import React, { useState } from 'react';
+
+
 class SingleMovie extends Component {
 
     state = {
